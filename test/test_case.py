325b44ba5a27bc4188ff4358f299e996822ad2f8
soderholmen <gustaf.soderholmen@gmail.com>
2018-01-23 08:49:12 +0100

    init com

---
+++ test/test_case.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+PKG = 'test'
+import roslib; roslib.load_manifest(PKG)
+import unittest 
+#from test.py import my_func
+
+class CaseA(unittest.TestCase):
+    def runTest(self):
+        my_var=False
+        
+        self.assertTrue(my_var)
+
+if __name__ == '__main__':
+    import rostest
+    rostest.rosrun(PKG, 'test_bare', CaseA)
